781d6fe35e2c4a253e0b47da8ccd5811b7b3ea11
BryanCruz <bryan.bialokur@gmail.com>
2021-02-27 21:54:54 -0300

    feat: shuffle initial deck

--- Assets/Scripts/ManageCartas.cs
+++ Assets/Scripts/ManageCartas.cs
@@ -21,17 +21,20 @@ public class ManageCartas : MonoBehaviour
 
 	void MostraCartas()
 	{
+		int[] arrayEmbaralhado = CriaArrayEmbaralhado();
+
 		// Instantiate( carta, new Vector3( 0, 0, 0 ), Quaternion.identity );
 		//  AddUmaCarta();
 
 		for ( int i = 0; i < 13; i++ )
 		{
-			AddUmaCarta( i );
+			// AddUmaCarta( i );
+			AddUmaCarta( i, arrayEmbaralhado[i] );
 		}
 	}
 
 
-	void AddUmaCarta(int rank)
+	void AddUmaCarta(int rank, int valor)
 	{
 		GameObject centro = GameObject.Find( "centroDaTela" );
 
@@ -44,12 +47,12 @@ public class ManageCartas : MonoBehaviour
 		// GameObject c = Instantiate( carta, new Vector3( 0, 0, 0 ), Quaternion.identity );
 		// GameObject c = Instantiate( carta, new Vector3( rank * 1.5f, 0, 0 ), Quaternion.identity );
 		GameObject c = Instantiate( carta, novaPosicao, Quaternion.identity );
-		c.tag = "" + rank;
-		c.name = "" + rank;
+		c.tag = "" + (valor + 1);
+		c.name = "" + valor;
 
 		string numeroCarta;
 
-		if ( rank == 0 )
+		/* if ( rank == 0 )
 			numeroCarta = "ace";
 		else if ( rank == 10 )
 			numeroCarta = "jack";
@@ -59,12 +62,45 @@ public class ManageCartas : MonoBehaviour
 			numeroCarta = "king";
 		else
 			numeroCarta = "" + (rank + 1);
+		*/ // if else para array ordenado no deck
+
+		if ( valor == 0 )
+			numeroCarta = "ace";
+		else if ( valor == 10 )
+			numeroCarta = "jack";
+		else if ( valor == 11 )
+			numeroCarta = "queen";
+		else if ( valor == 12 )
+			numeroCarta = "king";
+		else
+			numeroCarta = "" + (valor + 1);
 
 		string nomeDaCarta = numeroCarta + "_of_clubs";
 
 		Sprite s1 = Resources.Load<Sprite>( nomeDaCarta );
 		print( "S1: " + s1 );
 
-		GameObject.Find( "" + rank ).GetComponent<Tile>().SetCartaOriginal( s1 );
+		// GameObject.Find( "" + rank ).GetComponent<Tile>().SetCartaOriginal( s1 );
+		GameObject.Find( "" + valor ).GetComponent<Tile>().SetCartaOriginal( s1 );
+	}
+
+	public int[] CriaArrayEmbaralhado()
+	{
+		// cria um array para ser embaralhado
+		int[] novoArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+		// embaralha o array
+		for ( int t = 0; t < 13; t++ )
+		{
+			// pega um valor aleatorio r entre t e 13
+			int r = Random.Range( t, 13 );
+
+			// inverte as posições t e r do novo array
+			int temp = novoArray[t];
+			novoArray[t] = novoArray[r];
+			novoArray[r] = temp;
+		}
+
+		return novoArray;
 	}
 }
